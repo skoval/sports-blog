974cc2ec03e7a8a43de5426a8a6caca6f35a7d0d
skoval <s.a.kovalchik@gmail.com>
2017-08-18 19:25:04 +1000

    BP Plus next gen

---
+++ R/nextgen_bpplus.R
@@ -0,0 +1,34 @@
+library(dplyr)
+library(ggplot2)
+library(ggthemes)
+
+load("nextgen_bp_plus.RData")
+
+bp_plus$player <- factor(bp_plus$player, level = bp_plus$player[order(bp_plus$bp.plus.diff.mean)], order = T)	
+
+
+gg1 <- bp_plus %>%
+	ggplot(aes(y = bp.plus.diff.mean, ymin = bp.plus.diff.lower25, ymax = bp.plus.diff.upper25, x = player)) + 
+	geom_hline(yintercept = 0, color = "red") + 
+	geom_pointrange(size = 2, color = tableau_color_pal()(1)) + 
+	theme_bw()	+
+	scale_y_continuous("Differential") + 
+	scale_x_discrete("") + 
+	coord_flip() + 
+	theme(text = element_text(size = 16)) + 
+	ggtitle("BP Plus")
+
+bp_plus$player <- factor(bp_plus$player, level = bp_plus$player[order(bp_plus$bp.plus.chance.mean)], order = T)	
+		
+gg2 <- bp_plus %>%
+	ggplot(aes(y = bp.plus.chance.mean, ymin = bp.plus.chance.lower25, ymax = bp.plus.chance.upper25, x = player)) + 
+	geom_hline(yintercept = 0, color = "red") + 
+	geom_pointrange(size = 2, color = tableau_color_pal()(1)) + 
+	theme_bw()	+
+	scale_y_continuous("Differential") + 
+	scale_x_discrete("") + 
+	coord_flip() + 
+	theme(text = element_text(size = 16)) + 
+	ggtitle("BP Plus Opportunities")
+
+gg2	
--- R/tourney_strength.R
+++ R/tourney_strength.R
@@ -38,23 +38,25 @@ tourney_strength <- tourney_strength %>%
 	
 tourney_strength$level <- factor(tourney_strength$level, level = c("GS", "1000", "500", "250"), order = T)	
 
-month_labels <- tourney_strength %>%
-	dplyr::mutate(
-		month = month(start_date)
-	) %>%
-	group_by(month) %>%
-	dplyr::summarise(
-		start_date = ymd(paste(year(start_date[1]), month(start_date[1]), 1, sep = "-")),
-		day = yday(start_date)
-	)
-	
+format_tournament <- function(x){
+
+	x <- gsub("-", " ", x)
+	x <- strsplit(x, " ")
+	x <- sapply(x, function(x){
+		paste0(toupper(substr(x, 1, 1)), substr(x, 2, nchar(x)), collapse = " ")
+	})
+
+x
+}
+
 gg1 <- tourney_strength %>%
 	ggplot(aes(y = strength, x = yday(start_date), color = level, group = level))	 +
 	geom_point(size = 3)	 +
+	geom_text(aes(label = format_tournament(tourney_name)), nudge_y = 50) +
 	geom_segment(aes(x = yday(start_date), xend= yday(start_date), y=1800, yend = strength)) + 
 	facet_grid(level ~ .) +
 	scale_colour_tableau() + 
-	scale_y_continuous("Tournament Strength", lim = c(1800, 2300)) + 
+	scale_y_continuous("Tournament Strength", lim = c(1800, 2400)) + 
 	scale_x_continuous("Calendar Day") +	
 	theme_bw() + 
 	theme(legend.position = "none")
@@ -79,10 +81,11 @@ tourney_strength <- merge(tourney_strength, tourney_depth, by = "tourney_name")
 gg2 <- tourney_strength %>%
 	ggplot(aes(y = strength, x = depth, color = level))	 +
 	geom_point(size = 3)	 +
+	geom_text(aes(label = format_tournament(tourney_name))) +
 	geom_smooth(method = "lm", level = 0) + 
 	scale_colour_tableau(name = "Level") + 
 	scale_y_continuous("Tournament Strength") + 
-	scale_x_continuous("Tournament Depth") + 
+	scale_x_continuous("Tournament Depth", lim = c(100, 900)) + 
 	theme_bw() 
 
 gg2	
